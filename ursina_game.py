c886a2a7621e6e8c35df7aa9b3dc3bf7955595f4
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-06-17 08:48:56 -0400

    cameras now in top quarters; cards will be in the bottom

--- ursina_game.py
+++ ursina_game.py
@@ -3,7 +3,7 @@ from ursina.shaders import lit_with_shadows_shader
 
 app = Ursina()
 cycle = Entity(model = "motorcycle/motor", rotation = (0, 90, 0))
-cycle2 = Entity(model = "motorcycle/motorBlue", position = (10, 0, 0))
+cycle2 = Entity(model = "motorcycle/motorBlue", position = (10, 0, 0), rotation = (0, 90, 0))
 start = Entity(model = "racing/roadStart", position = (0, -2.25, 0), scale = 10)
 sun = DirectionalLight()
 sun.look_at(Vec3(1, -1, 1))
@@ -25,17 +25,16 @@ base.disableMouse()
 
 def createCamera(self, dispRegion):
     theCamera = base.makeCamera(base.win, displayRegion = dispRegion)
-    theCamera.node().getLens().setAspectRatio(3.0/4.0)
     theCamera.setPos(0, 0, -10)
     return theCamera
 
-base.camera1 = createCamera(base, (0.5, 1.0, 0, 1))
+base.camera1 = createCamera(base, (0, 0.5, 0.5, 1))
 base.camera1.reparentTo(cycle)
 base.camera1.lookAt(cycle)
 base.camera1.setPos(15, 2, 0)
 base.camera1.setHpr(90, 0, 0)
 
-base.camera2 = createCamera(base, (0.0, 0.5, 0, 1))
+base.camera2 = createCamera(base, (0.5, 1, 0.5, 1))
 base.camera2.reparentTo(cycle2)
 base.camera2.lookAt(cycle2)
 base.camera2.setPos(15, 2, 0)

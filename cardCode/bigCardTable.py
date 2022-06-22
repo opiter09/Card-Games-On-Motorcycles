e19e7cf9e072b89392008328f3523583bd72395c
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-06-22 16:04:14 -0400

    clearer variables, began card table

---
+++ cardCode/bigCardTable.py
@@ -0,0 +1,15 @@
+"bigTable" = {
+    "Killer Clown": {
+        "cost": "3EE",
+        "type": "Comrade",
+        "subType": "Clown",
+        "strength": 5,
+        "vitality": 5,
+        "effects": [ 
+            "1E, Contract: If you have an unoccupied Auxiliary Zone, terminate target monster and create a Pod token, an Alien Aether with /
+            \'At the beginning of your maintence phase, terminate this. If you have an unoccupied Comrade Zone, create a token copy /
+            of target comrade you control named Killer Clown.\' Activate this ability only during your turn, before the combat phase."
+        ]
+    }
+}
+     

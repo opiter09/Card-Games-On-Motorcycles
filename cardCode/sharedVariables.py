b4b626bd6bad30c5d812d47c5874a09e72f7b89a
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-06-22 16:47:51 -0400

    typo fix

--- cardCode/sharedVariables.py
+++ cardCode/sharedVariables.py
@@ -1,16 +1,19 @@
 import json
 import numpy
+import random
 
 f = open("player1.txt")
 player1Deck = json.load(f)
 f.close()
 player1Deck = numpy.array(list(player1Deck.values()))
+random.shuffle(player1Deck)
 
 f = open("player2.txt")
 player2Deck = json.load(f)
 f.close()
 player2Deck = numpy.array(list(player2Deck.values()))
 #print(player2Deck[0])
+random.shuffle(player2Deck)
 
 p1Life = 15
 p2Life = 15

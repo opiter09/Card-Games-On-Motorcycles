8aea09f82cfe308c42b83ecbe4813f288cd4b90a
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-06-25 00:05:37 -0400

    tooltips for hand finalized

--- cardCode/bigCardTable.py
+++ cardCode/bigCardTable.py
@@ -3,11 +3,11 @@ bigTable = {
         "cost": "3CC",
         "type": "Comrade",
         "subType": "Clown",
-        "strength": 5,
-        "vitality": 5,
-        "effects": [ "Contract and 1C: If you have an unoccupied Auxiliary Zone, terminate target monster and create a Pod token, a Cosmic Aether with\
-        'At the beginning of your maintence phase, terminate this. If you have an unoccupied Comrade Zone, create a token copy\
-        of target comrade you control named Killer Clown.' Activate this ability only during your turn, before the combat phase." ]
+        "strength": 4,
+        "vitality": 4,
+        "effects": [ "Contract and 1C: If you have an unoccupied Auxiliary Zone, terminate target monster and create a Pod token, a Cosmic Aether with \
+'At the beginning of your maintence phase, terminate this. If you have an unoccupied Comrade Zone, create a token copy \
+of target comrade you control named Killer Clown.' Activate this ability only during your turn, before the combat phase." ]
     }
 }
      
--- cardCode/cardTooltips.py
+++ cardCode/cardTooltips.py
@@ -7,15 +7,19 @@ for i in [1, 2]:
     for j in [1, 2, 3, 4, 5, 6]:
         if (j <= len(shared["player" + str(i) + "Hand"])):
             entry = bigTable[shared["player" + str(i) + "Hand"][j - 1]]
-            text = shared["player" + str(i) + "Hand"][j - 1] + "     " + entry["cost"] + "\n" + entry["type"]
+            text = shared["player" + str(i) + "Hand"][j - 1] + "     " + entry["cost"] + "\n"
+            text = text + "Strength: " + str(entry["strength"]) + "  Vitality: " + str(entry["vitality"])
+            text = text + "\n" + entry["type"]
             if (entry["type"] == "Comrade"):
                 text = text + "~" + entry["subType"]
+            effectsLength = 0
             for k in range(len(entry["effects"])):
                 text = text + "\n" + entry["effects"][k]
-
+                effectsLength = effectsLength + len(entry["effects"][k])
             if (i == 1):
+                sizeFactor = effectsLength / 79.15
                 globals()["p" + str(i) + "hand" + str(j) + "Tooltip"] = Button(scale = 0.08, position = (-0.19 - (j / 10), -0.42))
-                globals()["p" + str(i) + "hand" + str(j) + "Tooltip"].tooltip = Tooltip(text, origin = (4.5, 0.55), ignore = True, background = False)
+                globals()["p" + str(i) + "hand" + str(j) + "Tooltip"].tooltip = Tooltip(text, scale = 0.95, world_x = -17.5, world_y = -2.3 - sizeFactor, ignore = True, background = False)
             else:
                 globals()["p" + str(i) + "hand" + str(j) + "Tooltip"] = Button(scale = 0.08, position = (0.19 + (j / 10), -0.07))
-                globals()["p" + str(i) + "hand" + str(j) + "Tooltip"].tooltip = Tooltip(text, origin = (-2.3, 1.1), ignore = True, background = False)            
+                globals()["p" + str(i) + "hand" + str(j) + "Tooltip"].tooltip = Tooltip(text, scale = 0.95, world_x = 5.5, world_y = -5.6 - sizeFactor, ignore = True, background = False)            

3a89ee4b19d914986db862ac5e3ba9f88be86caf
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-06-27 17:15:07 -0400

    made some cards

--- cardCode/bigCardTable.py
+++ cardCode/bigCardTable.py
@@ -5,15 +5,57 @@ bigTable = {
         "subType": "Clown",
         "strength": 4,
         "vitality": 4,
-        "effects": [ "Contract and 1C: If you have an unoccupied Auxiliary Zone, terminate target monster and create a Pod token, a Cosmic Aether with \
+        "effects": [ "Contract and 1C: If you have an unoccupied Auxiliary Zone, terminate target comrade and create a Pod token, a Cosmic Aether with \
 'At the beginning of your maintence phase, terminate this. If you have an unoccupied Comrade Zone, create a token copy \
-of target comrade you control named Killer Clown.' Activate this ability only during your turn, before the combat phase." ]
+of target comrade you control named Killer Clown.' Activate this effect only during your turn, before the combat phase." ]
     },
+
     "Foolish Charge": {
         "cost": "1A",
         "type": "Chant",
         "effects": [ "Play this card only during your opponent's maintenance phase.", "During this turn's combat phase, all of your opponent's \
 comrades get +2 Strength and must attack if able. At the beginning of this turn's end phase, terminate all comrades that attacked this turn." ]
-    }
+    },
+
+    "Oiled Stone": {
+        "cost": "1S",
+        "type": "Comrade",
+        "subType": "Boulder",
+        "strength": 0,
+        "vitality": 3,
+        "effects": [ "Oiled Stone cannot attack.", "Oiled Stone gets +10 strength while it has blaze counters on it.", "Whenever a card with blaze \
+counters on it damages Oiled Stone, put a blaze counter on Oiled Stone." ]
+    },
+
+    "Forest Fire": {
+        "cost": "2PP",
+        "type": "Aether",
+        "effects": [ "When there are no comrades in either territory, sacrifice Forest Fire.", "When Forest Fire enters your territory or \
+a comrade is terminated, put a blaze counter on Forest Fire, then deal damage to target comrade equal to the number of blaze counters on Forest Fire." ]
+    },
+    
+    "Trebuchet": {
+        "cost": "4M",
+        "type": "Aether",
+        "effects": [ "You cannot activate an effect of Trebuchet unless a comrade named \"Wheel Runner\" is in your territory.", "Sacrifice a \
+        Stone comrade: Trebuchet deals damage equal to its vitality to target comrade. If the sacrificed creature had blaze counters on it, Trebuchet \
+        also deals damage equal to its strength to the target comrade's controller."
+    
+    "Wheel Runner": {
+        "cost": "2A",
+        "type": "Comrade",
+        "subtype": "Laborer",
+        "strength": 1,
+        "vitality": 5,
+        "effects": []
+    },
+    
+    "ALien Observer": {
+        "cost": "C",
+        "type: Comrade",
+        "subtype": "Spy",
+        "strength": 1,
+        "vitality": 1,
+        "effects": [ "Alien Observer enters in your opponent's territory, not your own.", "Alien Observer cannot attack." ]
 }
      

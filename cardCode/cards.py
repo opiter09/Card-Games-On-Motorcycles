15f7fab24eda8bc46b296599dfbb79265f4526bf
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-06-27 12:05:44 -0400

    added check for deck size

--- cardCode/cards.py
+++ cardCode/cards.py
@@ -9,23 +9,37 @@ import cardCode.cardButtons as cardButtons
 import cardCode.buttonTooltips
 import cardCode.buttonMouseEvents
 
-#check = 0
-#for i in [1, 2]:
-    #counting = collections.Counter(sharedVariables.myGlobals()["player" + str(i) + "Deck"])
-    #for k in counting.values():
-        #if (k > 2):
-            #check = 1
-#if (check == 1):
-    #Text(text = "ILLEGAL DECK DETECTED. ONLY 2 COPIES OF A CARD ARE ALLOWED.", color = color.red, origin = (0, -10))
+check = 0
+check2 = 0
+for i in [1, 2]:
+    if (len(sharedVariables.myGlobals()["player" + str(i) + "Deck"]) != 40):
+        check2 = 1
+    counting = collections.Counter(sharedVariables.myGlobals()["player" + str(i) + "Deck"])
+    for k in counting.values():
+        if (k > 2):
+            check = 1
+if (check == 1):
+    Text(text = "ILLEGAL DECK DETECTED. ONLY 2 COPIES OF A CARD ARE ALLOWED.", color = color.red, origin = (0, -10))
+if (check2 == 1):
+    Text(text = "ILLEGAL DECK DETECTED. DECKS MUST HAVE EXACTLY 40 CARDS.", color = color.red, origin = (0, -9))
 
 def cardsUpdate():
-    #global check
-    #if (check >= 2):
-        #check = 0
-        #application.quit()
-    #elif (check != 0) and (check < 2):
-        #check = check + 0.2
-    sharedVariables.myGlobals()["player1Hand"][0] = "Foolish Charge"
+    global check
+    global check2
+
+    if (check >= 2):
+        check = 0
+        check2 = 0
+        application.quit()
+    elif (check != 0) and (check < 2):
+        check = check + 0.007
+
+    if (check2 >= 2):
+        check = 0
+        check2 = 0
+        application.quit()
+    elif (check2 != 0) and (check2 < 2):
+        check2 = check2 + 0.007
 
     cardText.updateText()
     cardSprites.updateSprites()
